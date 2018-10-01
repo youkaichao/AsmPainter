9ea02764d78d132fdc9a071d2211666ff0f792d1
AerysNan <nany16@163.com>
2018-10-01 14:12:48 +0800

    refactor to multiple files

--- main.asm
+++ main.asm
@@ -58,40 +58,6 @@ WndProc proc hWnd:HWND, uMsg:UINT, wParam:WPARAM, lParam:LPARAM
     ret 
 WndProc endp
 
-Render proc hWnd:HWND
-    ret
-Render endp
-
-LButtonDown proc hWnd:HWND,wParam:WPARAM,lParam:LPARAM
-    mov MouseClick,TRUE 
-    ret
-LButtonDown endp
-
-LButtonUp proc hWnd:HWND,wParam:WPARAM,lParam:LPARAM
-    mov MouseClick,FALSE
-    ret
-LButtonUp endp
-
-MouseMove proc hWnd:HWND,wParam:WPARAM,lParam:LPARAM
-    local hdc:HDC
-    local position:POINT
-    .IF !MouseClick
-        ret
-    .ENDIF
-    invoke GetDC,hWnd
-    mov hdc,eax
-    mov eax,lParam 
-    and eax,0FFFFh 
-    mov position.x,eax 
-    mov eax,lParam 
-    shr eax,16 
-    mov position.y,eax 
-    RGB 0,0,0
-    invoke SetPixel,hdc,position.x,position.y,eax
-    invoke ReleaseDC,hWnd,hdc
-    ret
-MouseMove endp
-
 start: 
     invoke GetModuleHandle, NULL 
     mov    hInstance,eax 
