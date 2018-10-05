d76abca5eff2fb5ac1239b7c80a1d3e90f15b52e
AerysNan <nany16@163.com>
2018-10-05 15:29:33 +0800

    enable buffer painter

--- function.asm
+++ function.asm
@@ -7,6 +7,12 @@ include function.inc
 .code 
 
 Render proc hWnd:HWND
+    local hdc:HDC
+    local ps:PAINTSTRUCT
+    invoke BeginPaint,hWnd,addr ps
+    mov hdc,eax
+    invoke BitBlt,hdc,0,0,1200,800,buffer,0,0,SRCCOPY
+    invoke EndPaint,hWnd,addr ps
     ret
 Render endp
 
@@ -28,13 +34,22 @@ LButtonUp endp
 
 MouseMove proc hWnd:HWND,wParam:WPARAM,lParam:LPARAM
     local hdc:HDC
+    local rect:RECT
     local hpen:HPEN
     local position:POINT
+    local bitmap:HBITMAP
     .IF !mouseClick
         ret
     .ENDIF
     invoke GetDC,hWnd
     mov hdc,eax
+    invoke CreateCompatibleDC,hdc
+    mov buffer,eax
+    invoke GetClientRect,hWnd,addr rect
+    invoke CreateCompatibleBitmap,hdc,rect.right,rect.bottom
+    mov bitmap,eax
+    invoke SelectObject,buffer,bitmap
+    invoke BitBlt,buffer,0,0,rect.right,rect.bottom,hdc,0,0,SRCCOPY
     mov eax,instruction
     .IF eax==PencilID
         RGB 0,0,0
@@ -44,7 +59,7 @@ MouseMove proc hWnd:HWND,wParam:WPARAM,lParam:LPARAM
         invoke CreatePen,PS_SOLID,10,eax
     .ENDIF
     mov hpen,eax
-    invoke SelectObject,hdc,hpen
+    invoke SelectObject,buffer,hpen
     mov eax,lParam 
     and eax,0FFFFh 
     mov position.x,eax 
@@ -53,11 +68,13 @@ MouseMove proc hWnd:HWND,wParam:WPARAM,lParam:LPARAM
     mov position.y,eax
     push position.x
     push position.y
-    invoke MoveToEx,hdc,mousePosition.x,mousePosition.y,0
-    invoke LineTo,hdc,position.x,position.y
-    invoke ReleaseDC,hWnd,hdc
+    invoke MoveToEx,buffer,mousePosition.x,mousePosition.y,0
+    invoke LineTo,buffer,position.x,position.y
     pop mousePosition.y
     pop mousePosition.x
+    invoke ReleaseDC,hWnd,hdc
+    invoke InvalidateRect,hWnd,0,FALSE
+    invoke UpdateWindow,hWnd
     ret
 MouseMove endp
 
