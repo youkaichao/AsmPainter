732a915137c1ff7f8dd26204b2c58bb2a8dd30fa
You Kaichao <youkaichao@gmail.com>
2018-10-06 10:18:47 +0800

    add menu

--- main.asm
+++ main.asm
@@ -6,26 +6,28 @@ include function.inc
 
 .code 
 
-WinMain proc hInst:HINSTANCE,hPrevInst:HINSTANCE,CmdLine:LPSTR,CmdShow:DWORD 
+WinMain proc hInst:HINSTANCE,hPrevInst:HINSTANCE,CmdLine:LPSTR,CmdShow:DWORD
+    extern hWndMainWindow:HWND
+    extern hWndCanvas:HWND
     local msg:MSG 
-    local hwnd:HWND
     mov ebx,OFFSET WindowProc
     invoke GetStockObject,GRAY_BRUSH
-    invoke CreateWindowClass,hInst,ebx,addr WindowClass,eax
+    invoke CreateWindowClass,hInst,ebx,addr WindowClass,eax,ID_FILE_MENU
     mov ebx,OFFSET CanvasProc
     invoke GetStockObject,WHITE_BRUSH
-    invoke CreateWindowClass,hInst,ebx,addr CanvasClass,eax
-    invoke CreateWindowEx,0,addr WindowClass,addr WindowName,WS_OVERLAPPEDWINDOW and not WS_MAXIMIZEBOX and not WS_THICKFRAME,0,0,1200,800,0,0,hInst,0 
-    mov   hwnd,eax 
-    invoke CreateWindowEx,0,addr ButtonClass,addr PencilName,WS_CHILD or WS_VISIBLE,0,0,100,50,hwnd,PencilID,hInst,0
-    invoke CreateWindowEx,0,addr ButtonClass,addr EraserName,WS_CHILD or WS_VISIBLE,0,50,100,50,hwnd,EraserID,hInst,0
-    invoke CreateWindowEx,0,addr CanvasClass,addr EraserName,WS_CHILD or WS_VISIBLE,100,0,1100,800,hwnd,0,hInst,0
-    invoke ShowWindow,hwnd,SW_SHOWNORMAL 
-    invoke UpdateWindow,hwnd
+    invoke CreateWindowClass,hInst,ebx,addr CanvasClass,eax,0
+    invoke CreateWindowEx,0,addr WindowClass,addr WindowName,WS_OVERLAPPEDWINDOW and not WS_MAXIMIZEBOX and not WS_THICKFRAME,0,0,1200,800,0,0,hInst,0  
+    mov hWndMainWindow,eax
+    invoke CreateWindowEx,0,addr ButtonClass,addr PencilName,WS_CHILD or WS_VISIBLE,0,0,100,50,hWndMainWindow,PencilID,hInst,0
+    invoke CreateWindowEx,0,addr ButtonClass,addr EraserName,WS_CHILD or WS_VISIBLE,0,50,100,50,hWndMainWindow,EraserID,hInst,0
+    invoke CreateWindowEx,0,addr CanvasClass,addr EraserName,WS_CHILD or WS_VISIBLE,100,0,1100,800,hWndMainWindow,0,hInst,0
+    mov hWndCanvas,eax
+    invoke ShowWindow,hWndMainWindow,SW_SHOWNORMAL 
+    invoke UpdateWindow,hWndMainWindow
     .WHILE TRUE 
-        invoke GetMessage, addr msg,NULL,0,0 
+        invoke GetMessage,addr msg,NULL,0,0 
         .BREAK .IF (!eax) 
-        invoke DispatchMessage, addr msg 
+        invoke DispatchMessage,addr msg 
     .ENDW 
     mov     eax,msg.wParam 
     ret 
@@ -34,10 +36,10 @@ WinMain endp
 
 
 start: 
-    invoke GetModuleHandle, NULL 
+    invoke GetModuleHandle,NULL 
     mov    hInstance,eax 
     invoke GetCommandLine
     mov commandLine,eax 
-    invoke WinMain, hInstance,NULL,commandLine, SW_SHOWDEFAULT 
+    invoke WinMain,hInstance,NULL,commandLine,SW_SHOWDEFAULT 
     invoke ExitProcess,eax
 end start 
--- resource.h
+++ resource.h
@@ -1,15 +1,18 @@
 //{{NO_DEPENDENCIES}}
-// Microsoft Visual C++ ï¿½ï¿½ï¿½ÉµÄ°ï¿½ï¿½ï¿½ï¿½Ä¼ï¿½ï¿½ï¿½
-// ï¿½ï¿½ Project.rc Ê¹ï¿½ï¿½
+// Microsoft Visual C++ Éú³ÉµÄ°üº¬ÎÄ¼þ¡£
+// ¹© Project.rc Ê¹ÓÃ
 //
 #define IDI_PAINTER                     10001
+#define ID_FILE_MENU                    40003
+#define ID_FILE_OPEN_MENU               40004
+#define ID_FILE_SAVE_MENU               40005
 
 // Next default values for new objects
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        102
-#define _APS_NEXT_COMMAND_VALUE         40001
+#define _APS_NEXT_RESOURCE_VALUE        103
+#define _APS_NEXT_COMMAND_VALUE         40006
 #define _APS_NEXT_CONTROL_VALUE         1001
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
