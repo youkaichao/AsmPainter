ce38a43a4228c0d9c53f2e4ed4df6f4c65841004
You Kaichao <youkaichao@gmail.com>
2018-10-07 19:23:59 +0800

    enable save file

--- function.asm
+++ function.asm
@@ -112,6 +112,9 @@ MouseLeave proc hWnd:HWND,wParam:WPARAM,lParam:LPARAM
     ret
 MouseLeave endp
 
+PBITMAPINFO       TYPEDEF PTR BITMAPINFO
+PBITMAPINFOHEADER TYPEDEF PTR BITMAPINFOHEADER
+
 FileOpenMenu proc hWnd:HWND
     local hdc:HDC
     local hdcBmp:HDC
@@ -159,7 +162,107 @@ FileOpenMenu proc hWnd:HWND
     ret
 FileOpenMenu endp
 
-FileSaveMenu proc hWnd:HWND
+FileSaveMenu proc USES edx ebx  hWnd:HWND
+    local hdc:HDC
+    local hdcBmp:HDC
+    local hBmpBuffer:HBITMAP
+    local pbi:PBITMAPINFO
+    local bmfHeader:BITMAPFILEHEADER   
+    local bi:BITMAPINFOHEADER   
+    local bmpScreen:BITMAP
+    local dwBmpSize:DWORD
+    local hDIB: HANDLE
+    local lpbitmap : PTR BYTE
+    local hFile:HANDLE  
+    local dwSizeofDIB:DWORD
+    local dwBytesWritten:DWORD
+    local rcClient:RECT
+
+    mov  ofn.lStructSize,SIZEOF ofn
+    mov  ofn.hwndOwner,NULL 
+    push hInstance 
+    pop  ofn.hInstance 
+    mov  ofn.lpstrFilter,OFFSET FilterString 
+    mov  ofn.lpstrFile,OFFSET fileNameBuffer 
+    mov  ofn.nMaxFile,SIZEOF fileNameBuffer 
+    mov  ofn.Flags,OFN_PATHMUSTEXIST
+    invoke GetSaveFileName,ADDR ofn
+    .IF (!eax)
+        ret
+    .ENDIF
+
+    invoke GetDC,hWndCanvas
+    mov hdc,eax
+    invoke CreateCompatibleDC,hdc
+    mov hdcBmp,eax
+    invoke GetClientRect,hWndCanvas,addr rcClient
+    invoke SetStretchBltMode,hdc,HALFTONE
+
+    mov ebx,rcClient.right
+    sub ebx,rcClient.left
+    mov edx,rcClient.bottom
+    sub edx,rcClient.top
+    invoke CreateCompatibleBitmap,hdc,ebx,edx
+    mov hBmpBuffer,eax
+    invoke SelectObject,hdcBmp,hBmpBuffer
+    invoke BitBlt,hdcBmp,0,0,ebx,edx,hdc,0,0,SRCCOPY
+    invoke GetObject,hBmpBuffer,SIZEOF BITMAP,addr bmpScreen
+    push sizeof BITMAPINFOHEADER
+    pop bi.biSize
+    push bmpScreen.bmWidth
+    pop bi.biWidth
+    push bmpScreen.bmHeight
+    pop bi.biHeight
+    mov bi.biPlanes,1
+    mov bi.biBitCount,32
+    mov bi.biCompression,BI_RGB
+    mov bi.biSizeImage,0
+    mov bi.biXPelsPerMeter,0
+    mov bi.biYPelsPerMeter,0
+    mov bi.biClrUsed,0
+    mov bi.biClrImportant,0
+
+    movzx eax,bi.biBitCount
+    mul bmpScreen.bmWidth
+    add eax,31
+    mov ebx,32
+    cdq
+    div ebx
+    mov edx,4
+    mul edx
+    mul bmpScreen.bmHeight
+    mov dwBmpSize,eax
+    invoke GlobalAlloc,GHND,dwBmpSize
+    mov hDIB,eax
+    invoke GlobalLock,hDIB
+    mov lpbitmap,eax
+
+    invoke GetDIBits,hdc,hBmpBuffer,0,bmpScreen.bmHeight,lpbitmap,addr bi,DIB_RGB_COLORS
+    invoke CreateFile,addr fileNameBuffer,GENERIC_WRITE,0,NULL,CREATE_ALWAYS,FILE_ATTRIBUTE_NORMAL,NULL
+    mov hFile,eax
+    mov eax,dwBmpSize 
+    add eax,sizeof BITMAPFILEHEADER
+    add eax,sizeof BITMAPINFOHEADER
+    mov dwSizeofDIB,eax
+
+    mov eax,sizeof BITMAPFILEHEADER
+    add eax,sizeof BITMAPINFOHEADER
+    mov bmfHeader.bfOffBits,eax
+    push dwSizeofDIB
+    pop bmfHeader.bfSize
+    mov bmfHeader.bfType,4D42h
+
+    invoke WriteFile,hFile,addr bmfHeader,sizeof BITMAPFILEHEADER,addr dwBytesWritten,NULL
+    invoke WriteFile,hFile,addr bi,sizeof BITMAPINFOHEADER,addr dwBytesWritten,NULL
+    invoke WriteFile,hFile,lpbitmap,dwBmpSize,addr dwBytesWritten,NULL
+
+    invoke GlobalUnlock,hDIB
+    invoke GlobalFree,hDIB
+    invoke CloseHandle,hFile
+
+    invoke DeleteDC,hdcBmp
+    invoke DeleteObject,hBmpBuffer
+    invoke ReleaseDC,hWndCanvas,hdc
     ret
 FileSaveMenu endp
 
