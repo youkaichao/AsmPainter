e5a178f21fcc8947dacc24a65ebd63218fbcabdc
AerysNan <nany16@163.com>
2018-10-12 00:03:59 +0800

    reset resource index

--- resource.h
+++ resource.h
@@ -2,43 +2,43 @@
 // Microsoft Visual C++ 生成的包含文件。
 // 供 Project.rc 使用
 //
-#define IDI_PAINTER                     104
-#define IDI_ERASER                      105
-#define IDI_PENCIL                      106
-#define IDC_ERASER                      107
-#define IDC_PENCIL                      108
-#define IDB_OPEN                        114
-#define IDB_SAVE                        115
-#define IDB_ERASER                      116
-#define IDB_PENCIL                      117
-#define IDR_TOOLBAR                     118
-#define IDR_ACCELERATOR                 120
-#define IDD_DIALOG                      121
-#define IDC_TEXT                        124
-#define IDC_EDIT                        1003
-#define ID_MENU                         40003
-#define ID_MENU_TOOLBAR_PENCIL          40008
-#define ID_MENU_TOOLBAR_ERASER          40009
-#define ID_OPEN_TOOLBAR                 40022
-#define ID_SAVE_TOOLBAR                 40023
-#define ID_PENCIL_TOOLBAR               40024
-#define ID_ERASER_TOOLBAR               40025
-#define ID_MENU_FILE_OPEN               40029
-#define ID_MENU_FILE_SAVE               40030
-#define ID_MENU_TOOLBAR_PALETTE_FOREGROUND 40036
-#define ID_MENU_TOOLBAR_PALETTE_BACKGROUND 40037
-#define ID_FOREGROUND_TOOLBAR           40038
-#define ID_BACKGROUND_TOOLBAR           40039
-#define ID_MENU_TOOLBAR_TEXT            40049
-#define ID_TEXT_TOOLBAR                 40050
+#define IDI_PAINTER                     101
+#define IDI_ERASER                      102
+#define IDI_PENCIL                      103
+#define IDC_ERASER                      104
+#define IDC_PENCIL                      105
+#define IDB_OPEN                        106
+#define IDB_SAVE                        107
+#define IDB_ERASER                      108
+#define IDB_PENCIL                      109
+#define IDR_TOOLBAR                     110
+#define IDR_ACCELERATOR                 111
+#define IDD_DIALOG                      112
+#define IDC_TEXT                        113
+#define IDC_EDIT                        1001
+#define ID_MENU                         40001
+#define ID_MENU_TOOLBAR_PENCIL          40002
+#define ID_MENU_TOOLBAR_ERASER          40003
+#define ID_OPEN_TOOLBAR                 40004
+#define ID_SAVE_TOOLBAR                 40005
+#define ID_PENCIL_TOOLBAR               40006
+#define ID_ERASER_TOOLBAR               40007
+#define ID_TEXT_TOOLBAR                 40008
+#define ID_MENU_FILE_OPEN               40009
+#define ID_MENU_FILE_SAVE               40010
+#define ID_MENU_TOOLBAR_PALETTE_FOREGROUND 40011
+#define ID_MENU_TOOLBAR_PALETTE_BACKGROUND 40012
+#define ID_MENU_TOOLBAR_TEXT            40013
+#define ID_FOREGROUND_TOOLBAR           40014
+#define ID_BACKGROUND_TOOLBAR           40015
 
 // Next default values for new objects
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        125
-#define _APS_NEXT_COMMAND_VALUE         40051
-#define _APS_NEXT_CONTROL_VALUE         1005
+#define _APS_NEXT_RESOURCE_VALUE        114
+#define _APS_NEXT_COMMAND_VALUE         40016
+#define _APS_NEXT_CONTROL_VALUE         1002
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
 #endif
